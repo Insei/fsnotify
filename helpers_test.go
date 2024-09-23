acec45ff04f5d2dee946f0bb25b29ad8fb4cddd5
Konstantin Gamayunov <ksgamayunov@gmail.com>
2024-09-23 15:05:06 +0300

    all github.com/fsnotify/fsnotify imports replaced with github.com/Insei/fsnotify

--- helpers_test.go
+++ helpers_test.go
@@ -13,8 +13,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/fsnotify/fsnotify/internal"
-	"github.com/fsnotify/fsnotify/internal/ztest"
+	"github.com/Insei/fsnotify/internal"
+	"github.com/Insei/fsnotify/internal/ztest"
 )
 
 // We wait a little bit after most commands; gives the system some time to sync
